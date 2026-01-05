ffa453393bba801a6f4890c599b9bb7124d5c4a7
watthanachai <watthanachai.b@kkumail.com>
2026-01-05 16:04:55 +0700

    commit it

---
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div style="text-align:center; margin-top:16px;">
+        <h2>Photo Gallery</h2>
+        <p>รหัสนักศึกษา: 663380232-2</p>
+        <p>ชื่อ-นามสกุล: วัฒนชัย บึงจันทร์</p>
+      </div>
+
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto">
+        <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <!-- CHANGE: Remove or comment out <ExploreContainer /> -->
+      <!-- <ExploreContainer name="Tab 2 page" /> -->
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+// CHANGE: Add import from `ionicons/icons`
+import { camera } from 'ionicons/icons';
+// CHANGE: Update import from `@ionic/vue` to include necessary Ionic components
+import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+// CHANGE: Remove or comment out the ExploreContainer import
+// import ExploreContainer from '@/components/ExploreContainer.vue';
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+const { takePhoto } = usePhotoGallery();
+</script>
