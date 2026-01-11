9e7a075c4ef441a3d8854ab926e09247b7e02c3e
watthanachai <watthanachai.b@kkumail.com>
2026-01-11 17:17:54 +0700

    commit

--- apps/lab04-photo-vue/src/views/Tab2Page.vue
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -1,41 +1,57 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Photo Gallery</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Photo Gallery</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Photo Gallery</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Photo Gallery</ion-title>
+                </ion-toolbar>
+            </ion-header>
 
-      <div style="text-align:center; margin-top:16px;">
-        <h2>Photo Gallery</h2>
-        <p>รหัสนักศึกษา: 663380232-2</p>
-        <p>ชื่อ-นามสกุล: วัฒนชัย บึงจันทร์</p>
-      </div>
-      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
-        <!-- CHANGE: Add a click event listener to the floating action button. -->
-        <ion-fab-button @click="addNewToGallery()">
-          <ion-icon :icon="camera"></ion-icon>
-        </ion-fab-button>
-      </ion-fab>
-      <!-- CHANGE: Remove or comment out <ExploreContainer /> -->
-      <!-- <ExploreContainer name="Tab 2 page" /> -->
-    </ion-content>
-  </ion-page>
+            <!-- <ExploreContainer name="Tab 2 page" /> -->
+            <div class="ion-padding">
+                <p>โดย นายวัฒนชัย บึงจันทร์ 663380232-2</p>
+            </div>
+
+            <ion-grid>
+                <ion-row>
+                    <ion-col size="6" :key="photo" v-for="photo in photos">
+                        <ion-img :src="photo.webviewPath"></ion-img>
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+
+            <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+                <ion-fab-button @click="takePhoto()">
+                    <ion-icon :icon="camera"></ion-icon>
+                </ion-fab-button>
+            </ion-fab>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script setup lang="ts">
-import { camera } from 'ionicons/icons';
-import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+} from "@ionic/vue";
+import { camera } from "ionicons/icons";
 
-// CHANGE: Add `usePhotoGallery` import
-import { usePhotoGallery } from '@/composables/usePhotoGallery';
+import { usePhotoGallery } from "@/composables/usePhotoGallery";
 
-// CHANGE: Destructure `addNewToGallery` from `usePhotoGallery()
-const { addNewToGallery } = usePhotoGallery();
+const { photos, takePhoto } = usePhotoGallery();
 </script>
